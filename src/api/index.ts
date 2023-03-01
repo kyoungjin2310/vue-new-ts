96d88cbdb79aa35a992c8dff072aa1da913a383c
kyoungjin2310 <kyoungjin2310@gmail.com>
2023-03-01 23:30:09 +0900

    feat: [ts-axios] axios에 typescript 적용

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,31 @@
-import axios from 'axios';
+import axios, { AxiosPromise } from "axios";
 
 const api = {
-  news: 'https://api.hnpwa.com/v0/news/1.json',
-  ask: 'https://api.hnpwa.com/v0/ask/1.json',
-  jobs: 'https://api.hnpwa.com/v0/jobs/1.json',
-  user: 'https://api.hnpwa.com/v0/user/',
-  item: 'https://api.hnpwa.com/v0/item/',
+  news: "https://api.hnpwa.com/v0/news/1.json",
+  ask: "https://api.hnpwa.com/v0/ask/1.json",
+  jobs: "https://api.hnpwa.com/v0/jobs/1.json",
+  user: "https://api.hnpwa.com/v0/user/",
+  item: "https://api.hnpwa.com/v0/item/",
 };
 
-function fetchNews() {
+//axios response data를 복사하거나 swagger 값 가져와서 type정의
+//data가져와서type 정의 사이트가서 복붙해도됨
+
+interface NewsItem {
+  comments_count: number;
+  domain: string;
+  id: number;
+  points: number;
+  time: number;
+  time_ago: string;
+  title: string;
+  type: string;
+  url: string;
+  user: string;
+}
+
+//AxiosResponse 쓰기도 함, AxiosPromise는 몇개 안적음
+function fetchNews(): AxiosPromise<NewsItem[]> {
   return axios.get(api.news);
 }
 
