2a696a196c2d0f769c20bb81cf5c6bec1d89277d
kyoungjin2310 <kyoungjin2310@gmail.com>
2023-04-03 22:55:05 +0900

    feat:[mutation-ts] mutation에 ts 적용

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,20 +1,40 @@
-export default {
-  SET_NEWS(state, news) {
+import { NewsItem } from "@/api";
+import { RootState } from "./state";
+
+//ts - enum : 열거형
+enum MutationTypes {
+  SET_NEWS = "SET_NEWS",
+}
+
+const mutations = {
+  [MutationTypes.SET_NEWS](state: RootState, news: NewsItem[]) {
     state.news = news;
   },
-  SET_ASK(state, ask) {
-    state.ask = ask;
-  },
-  SET_JOBS(state, jobs) {
-    state.jobs = jobs;
-  },
-  SET_USER(state, user) {
-    state.user = user;
-  },
-  SET_ITEM(state, item) {
-    state.item = item;
-  },
-  SET_LIST(state, list) {
-    state.list = list;
-  },
 };
+
+//mutations type을 store에서 추론이 가능하게 만들기
+//Mutations에 s를 빼먹으면 안됨, vuex에 이미 Mutation가 있음
+type Mutations = typeof mutations;
+
+export { mutations, Mutations };
+
+// export default {
+//   SET_NEWS(state, news) {
+//     state.news = news;
+//   },
+//   SET_ASK(state, ask) {
+//     state.ask = ask;
+//   },
+//   SET_JOBS(state, jobs) {
+//     state.jobs = jobs;
+//   },
+//   SET_USER(state, user) {
+//     state.user = user;
+//   },
+//   SET_ITEM(state, item) {
+//     state.item = item;
+//   },
+//   SET_LIST(state, list) {
+//     state.list = list;
+//   },
+// };
