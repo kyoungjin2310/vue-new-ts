4c316f4e6354a953c55d8b4cec233078c5a97ce6
kyoungjin2310 <kyoungjin2310@gmail.com>
2023-04-04 19:22:29 +0900

    feat:[mutations - ts] mutations 뷰엑스 커스텀 타입 정의

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import Vuex, { StoreOptions } from "vuex";
 import getters from "@/store/getters";
-import mutations from "@/store/mutations";
+import { mutations } from "@/store/mutations";
 import actions from "@/store/actions";
 import { RootState, state } from "./state";
 
