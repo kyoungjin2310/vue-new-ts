f01a420b45bf35edacae59ae8526dcf6a7409cb2
kyoungjin2310 <kyoungjin2310@gmail.com>
2023-04-04 19:33:37 +0900

    feat: [ts-Omit] mutations 정의 방법

--- src/store/types.ts
+++ src/store/types.ts
@@ -11,5 +11,7 @@ type MyMutations = {
   ): ReturnType<Mutations[K]>;
 };
 
-//& - 타입 합집합 - Omit & MyMutations
+//& - 타입 합집합 - Omit & MyMutations(하나라도 빠지면 오류)
+//Omit - Omit<객체, 제외키> : 타입 정의중에서 특정 키만 제외하겠다는 의미
+//commit은 나중에 정의 되고, MyMutations에서 commit을 새로 정의하기 때문에 Omit을 사용
 export type MyStore = Omit<Store<RootState>, "commit"> & MyMutations;
